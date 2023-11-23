c095c1830f8aa4cc5277063a39986553953307b1
Dimi Tsonev <dconev@gmail.com>
2023-11-23 10:03:31 +0000

    filtered user choise

--- starter/script.js
+++ starter/script.js
@@ -122,21 +122,23 @@ function getPasswordOptions() {
   let isNumbers = confirm("Would you like your password to contain numbers");
   let isSpecialChar = confirm("Would you like your password to contain special characters" );
 
-  return {
-    passwordLength,
-    isLowerCase,
-    isUpperCase,
-    isNumbers,
-    isSpecialChar,
-  }
+  const userChoice = [
+    { key: 'passwordLength', value: passwordLength },
+    { key: 'isLowerCase', value: isLowerCase },
+    { key: 'isUpperCase', value: isUpperCase },
+    { key: 'isNumbers', value: isNumbers },
+    { key: 'isSpecialChar', value: isSpecialChar }
+  ];
+  return userChoice
+  
 }
 
+const userChoices = getPasswordOptions();
+console.log(userChoices)
 
-// console.log (typeof passwordOption)
-// console.log(passwordOption.passwordLength);
-// console.log(passwordOption.isLowerCase);
+const filteredChoices = userChoices.filter((userChoice) => userChoice.value);
 
-
+console.log(filteredChoices);
 
 // Function for getting a random element from an array
 
@@ -144,43 +146,59 @@ Array.prototype.getRandom = function(){
   return this[Math.floor(Math.random()*this.length)];
 }
 
+// const passwordOption = getPasswordOptions();
+// console.log(passwordOption)
+// console.log(passwordOption.isLowerCase)
+// console.log(passwordOption.passwordLength)
+// console.log(passwordArrays.specialCharacters.getRandom())
+
+
 // console.log(Object.values(passwordArrays.specialCharacters.getRandom()))
 
 
 // Function to generate password with user input
 
-function generatePassword() {
-  const passwordOption = getPasswordOptions();
-  let password = '';
 
-  for (let i = 0; i < passwordOption.passwordLength; i++) {
-    let tempPassword = '';
 
-    if (passwordOption.isLowerCase) {
-      tempPassword = passwordArrays.lowerCasedCharacters.getRandom();
-      console.log(tempPassword)
-    } else if (passwordOption.isUpperCase) {
-      tempPassword = passwordArrays.upperCasedCharacters.getRandom();
-      console.log(tempPassword)
 
-    } else if (passwordOption.isNumbers) {
-      tempPassword = passwordArrays.numericCharacters.getRandom();
+// function generatePassword() {
+//   const passwordOption = getPasswordOptions();
+//   let password = '';
+//   console.log('password options ->',passwordOption)
+
+//   for (let i = 0; i < passwordOption.passwordLength; i++) {
+//     let tempPassword = '';
+//     if (passwordOption.isLowerCase) {
+//       tempPassword = passwordArrays.lowerCasedCharacters.getRandom();
+//       console.log('temp password',tempPassword)
+//       password+=tempPassword 
+
+//       console.log('line 169, password',password)
+
+//     } else if (passwordOption.isUpperCase) {
+//       tempPassword = passwordArrays.upperCasedCharacters.getRandom();
+//       console.log("temp password line 175",tempPassword)
+//       password+=tempPassword 
+//       console.log('line 177, password',password)
+
+//     } else if (passwordOption.isNumbers) {
+//       tempPassword = passwordArrays.numericCharacters.getRandom();
       
 
-    } else {
-      tempPassword = passwordArrays.specialCharacters.getRandom();
+//     } else {
+//       tempPassword = passwordArrays.specialCharacters.getRandom();
       
 
-    }
+//     }
 
-    password += tempPassword;
-    console.log('final pass',password)
-  }
-  console.log(password)
-  return password;
+//     // password;
+//     console.log('final pass',password)
+//   }
+//   console.log(password)
+//   return password;
   
-}
-generatePassword() 
+// }
+// generatePassword()
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
